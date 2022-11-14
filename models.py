b28722925ee72a8f856e03ed00c91a0954b65232
hannatrap <107367091+hannatrap@users.noreply.github.com>
2022-11-13 18:14:21 -0600

    Add files via upload

---
+++ models.py
@@ -0,0 +1,33 @@
+"""Models for Cupcake app."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+DEFAULT_IMAGE_URL = "https://thestayathomechef.com/wp-content/uploads/2017/12/Most-Amazing-Chocolate-Cupcakes-1-small.jpg"
+
+
+
+class Cupcake(db.Model):
+    """cupcake model"""
+
+    __tablename__ = "cupcakes"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    flavor = db.Column(db.Text, nullable=False)
+    size = db.Column(db.Text, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    image = db.Column(db.Text, nullable = False, default = DEFAULT_IMAGE_URL)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "flavor": self.flavor,
+            "size": self.size,
+            "rating": self.rating,
+            "image": self.image,
+        }
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
